402c33064a1270615facd293dda2c90cb2ca42c7
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-01 21:20:17 +0200

    authcontext

---
+++ wine-frontend/src/context/AuthContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  if (token) {
+    localStorage.setItem("token", token);
+  }
+
+  return (
+    <AuthContext.Provider value={{ token, setToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- wine-frontend/src/pages/index.js
+++ wine-frontend/src/pages/index.js
@@ -1,2 +1,3 @@
-export { default as Home } from "./Home/Home.jsx";
-export { default as About } from "./About/About.jsx";
+export { default as Wines } from "./Wines/Wines.jsx";
+export { default as Login } from "./Login/Login.jsx";
+export { default as Register } from "./Register/Register.jsx";
