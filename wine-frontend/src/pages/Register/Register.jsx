f4c9bd7430ee615084a6c48bce61e305f584fd42
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-01 21:49:48 +0200

    register working

--- wine-frontend/src/pages/Register/Register.jsx
+++ wine-frontend/src/pages/Register/Register.jsx
@@ -1,23 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Section, Heading, InputField, Button } from "../../components";
 import * as S from "./Register.style";
 
+function newUser(email, password, history, setNotification) {
+  console.log(email);
+  fetch(`http://localhost:8080/register`, {
+    method: "Post",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.msg === "User has been succesfully registered") {
+        history.push("/");
+      } else {
+        setNotification(data.msg);
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
 function Register() {
+  const [notification, setNotification] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const history = useHistory();
+
   return (
     <Section>
       <Heading>Register</Heading>
-      <form>
+      {notification && <p>{notification}</p>}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          newUser(email, password, history, setNotification);
+        }}
+      >
         <InputField
-          type="text"
+          type="email"
           labelText="Email"
           placeholder="Email"
-          handlechange={() => console.log("yay")}
+          handleChange={(e) => setEmail(e.target.value)}
         />
         <InputField
           type="password"
           labelText="Password"
           placeholder="Password"
-          handleChange={() => console.log("yay")}
+          handleChange={(e) => setPassword(e.target.value)}
         />
         <S.ButtonBlock>
           <Button type="submit">REGISTER</Button>
