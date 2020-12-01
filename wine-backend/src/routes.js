10dbe87582f7b9f2720fea85b722119091792bbd
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-01 23:11:08 +0200

    login working

--- wine-backend/src/routes.js
+++ wine-backend/src/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const middleware = require("./middleware");
 const con = require("./db");
 
@@ -49,6 +49,58 @@ router.post("/register", middleware.userValidation, (req, res) => {
             );
           }
         });
+      }
+    }
+  );
+});
+
+router.post("/login", middleware.userValidation, (req, res) => {
+  const email = req.body.email;
+  con.query(
+    `SELECT * FROM users WHERE email = ${mysql.escape(email)}`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ msg: "Internal server error gathering user details" });
+      } else if (result.length !== 1) {
+        return res.status(400).json({
+          msg: "The provided details are incorrect or the user does not exist",
+        });
+      } else {
+        bcrypt.compare(
+          req.body.password,
+          result[0].password,
+          (bErr, bResult) => {
+            if (bErr || !bResult) {
+              return res.status(400).json({
+                msg:
+                  "The provided details are incorrect or the user does not exist",
+              });
+            } else if (bResult) {
+              const token = jwt.sign(
+                {
+                  userId: result[0].id,
+                  email: result[0].email,
+                },
+                process.env.SECRET_KEY,
+                {
+                  expiresIn: "7d",
+                }
+              );
+
+              return res.status(200).json({
+                msg: "Logged In",
+                token,
+                userData: {
+                  userId: result[0].id,
+                  email: result[0].email,
+                },
+              });
+            }
+          }
+        );
       }
     }
   );
--- wine-frontend/src/pages/Login/Login.jsx
+++ wine-frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,79 @@
-import React from "react";
-import { Section } from "../../components";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import {
+  Section,
+  Heading,
+  InputField,
+  Button,
+  Notification,
+} from "../../components";
+import { AuthContext } from "../../context/AuthContext";
+import * as S from "./Login.style";
 
-function About() {
-  return <Section>Login</Section>;
+function loginUser(email, password, history, setNotification, auth) {
+  fetch(`http://localhost:8080/login`, {
+    method: "Post",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.token) {
+        auth.setToken(data.token);
+        history.push("/");
+      } else {
+        return setNotification(data.msg || "Error");
+      }
+    })
+    .catch((err) => setNotification(err.message));
 }
 
-export default About;
+function Login() {
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const [notification, setNotification] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  return (
+    <Section>
+      <Heading>Login</Heading>
+      {notification && (
+        <Notification color="error" handleChange={() => setNotification(false)}>
+          {notification}
+        </Notification>
+      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          loginUser(email, password, history, setNotification, auth);
+        }}
+      >
+        <InputField
+          type="email"
+          labelText="Email"
+          placeholder="Email"
+          handleChange={(e) => setEmail(e.target.value)}
+        />
+        <InputField
+          type="password"
+          labelText="Password"
+          minLength="8"
+          maxLength="64"
+          placeholder="Password"
+          handleChange={(e) => setPassword(e.target.value)}
+        />
+        <S.ButtonBlock>
+          <Button type="submit">LOGIN</Button>
+        </S.ButtonBlock>
+      </form>
+    </Section>
+  );
+}
+
+export default Login;
