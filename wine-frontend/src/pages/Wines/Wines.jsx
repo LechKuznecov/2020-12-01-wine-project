96feda5ac27ba5a2080e125b5fec69bd66d07b30
Petras Slekys <Petras.Slekys@ibm.com>
2020-12-02 21:08:11 +0200

    Wine Type Get and display

--- wine-frontend/src/pages/Wines/Wines.jsx
+++ wine-frontend/src/pages/Wines/Wines.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Section } from "../../components";
+import { AuthContext } from "../../context/AuthContext";
 
 function Wines() {
+  const [data, setData] = useState();
+  const auth = useContext(AuthContext);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/viewwinetypes", {
+      headers: {
+        Authorization: `Bearer ${auth.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <>
+      {console.log(data)}
       <Section background="grey">Wines</Section>
     </>
   );
