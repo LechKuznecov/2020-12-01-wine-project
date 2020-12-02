01874e7dd61873e75bc22b7af6412f04e704f01e
Petras Slekys <Petras.Slekys@ibm.com>
2020-12-02 21:01:30 +0200

    changed display of menu when loggedIn

--- wine-frontend/src/components/Header/Header.jsx
+++ wine-frontend/src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import * as S from "./Header.style";
 
-function Header() {
+function Header({ isLoggedIn }) {
   return (
     <S.Header>
       <S.Wrapper>
         <S.Logo>winefine</S.Logo>
 
         <S.Actions>
-          <S.StyledLink to="/">Wine Type</S.StyledLink>
-          <S.StyledLink to="/addWineType">Add Wine Type</S.StyledLink>
-          <S.StyledLink to="/list">Wine List</S.StyledLink>
-          <S.StyledLink to="/addWine">Add Wine</S.StyledLink>
-          <S.StyledLink to="/login">Login</S.StyledLink>
-          <S.StyledLink to="/register">Register</S.StyledLink>
-          <S.StyledLink to="/logout">Logout</S.StyledLink>
+          {isLoggedIn && (
+            <>
+              <S.StyledLink to="/">Wine Type</S.StyledLink>
+              <S.StyledLink to="/addWineType">Add Wine Type</S.StyledLink>
+              <S.StyledLink to="/list">Wine List</S.StyledLink>
+              <S.StyledLink to="/addWine">Add Wine</S.StyledLink>
+              <S.StyledLink to="/logout">Logout</S.StyledLink>
+            </>
+          )}
+          {!isLoggedIn && (
+            <>
+              <S.StyledLink to="/login">Login</S.StyledLink>
+              <S.StyledLink to="/register">Register</S.StyledLink>
+            </>
+          )}
         </S.Actions>
       </S.Wrapper>
     </S.Header>
