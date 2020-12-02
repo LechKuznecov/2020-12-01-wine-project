b3b9f2558eadb9d08a432cc0ac3a1e0a2b512a15
Petras Slekys <Petras.Slekys@ibm.com>
2020-12-02 21:02:00 +0200

    minor bug fix

--- wine-backend/src/middleware.js
+++ wine-backend/src/middleware.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   isLoggedIn: (req, res, next) => {
     try {
-      const token = req.headers.authorization.split(".")[1];
+      const token = req.headers.authorization.split(" ")[1];
       const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
       req.userData = decodedToken;
       next();
